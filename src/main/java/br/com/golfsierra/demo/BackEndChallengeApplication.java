876809bf34a0152c4d47eb90746ae20ff72c0156
Gabriel Santino <ggs2209@gmail.com>
2023-08-02 14:02:46 -0300

    Project Created

---
+++ src/main/java/br/com/golfsierra/demo/BackEndChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.golfsierra.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/golfsierra/demo/BackEndChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.golfsierra.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
